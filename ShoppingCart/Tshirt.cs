af13645c1c275df7bf4111480357f544d51fb205
SineguguM <47022368+SineguguM@users.noreply.github.com>
2019-01-25 14:06:33 +0200

    [Added] T-shirt class

---
+++ ShoppingCart/Tshirt.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoppingCart
+{
+    public class Tshirts : Shirt
+    {
+        public override double SizeValue(int ShirtSize)
+        {
+            return base.SizeValue(ShirtSize);
+        }
+    }
+}
